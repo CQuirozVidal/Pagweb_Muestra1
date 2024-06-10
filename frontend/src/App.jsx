cc3438558ceffc2d6a1a84edc73b547d3d50ce9f
Camila Quiroz V <camilaquirozvidal001@gmail.com>
2024-06-09 22:15:45 -0400

    Setup Inicial: Sección Logo, Links y Menu Items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-  return(
-    <div>
-      <Navbar/>
-    </div>
-  ); 
+  return <div><Navbar/></div>
 };
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#f42c77",
+        primary: "#f42c37",
         secondary: "#f42c37",
         brandYellow: "#fdc62e",
         brandGreen: "#2dcc6f",
