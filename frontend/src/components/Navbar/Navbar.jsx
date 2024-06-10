b8dd93c51ea8c5030794f05b6b8f9f66aaec86e9
Camila Quiroz V <camilaquirozvidal001@gmail.com>
2024-06-10 00:26:26 -0400

    Navbar responsive con CSS

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { IoMdSearch } from 'react-icons/io';
+import { FaCartShopping } from "react-icons/fa6";
 
 const MenuLinks =[
     {
@@ -29,7 +31,8 @@ const  Navbar = () => {
         dark:bg-gray-900 
         dark:text-white duration-200 relative z-40'>
             <div className='py-4'>
-                <div className="container">
+                <div className="container flex justify-between
+                items-center">
                     {/*Logo and Links section */}
                     <div className='flex items-center gap-4'>
                         <a 
@@ -62,12 +65,38 @@ const  Navbar = () => {
                     </div>
                     
                     {/*Navbar Right section */}
-                    <div>
+                    <div className='flex justify-betweem 
+                    item-center gap-4'> 
                         {/*Search Bar Section */}
-                        <div>
-                           
+                        <div className='relative group hidden sm:block'>
+                            <input 
+                            type='text'
+                            placeholder='Search'
+                            className='
+                            search-bar
+                            '/>
+                            <IoMdSearch className='text-xl 
+                            text-gray-600 
+                            group-hover:text-primary
+                            dark:text-gray-400
+                            absolute top-1/2 -translate-y-1/2 right-3 
+                            duration-200'
+                            />
                         </div>
-
+                        {/*Order-button section*/}
+                        <button className='relative p-3'>
+                            <FaCartShopping  className='text-xl 
+                            text-gray-600 
+                            dark:text-gray-400'/>
+                            <div className='w-4 h-4 
+                            bg-red-500
+                            text-white
+                            rounded-full
+                            absolute top-0 right-0 flex 
+                            items-center justify-center text-xs'>
+                                4
+                            </div>
+                        </button>
                         {/*Dark Mode Section */}
                         <div>
 
